80770ab68854047c7f91e0a9dfb0570ca1b131f3
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-06 12:57:24 -0700

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'welcome/about'
+
+  namespace :api, defaults: { format: :json } do
+   resources :users
+  end
   
   root 'welcome#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
