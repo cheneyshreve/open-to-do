238dbffbd61c460f6dae0147419e92a21e0330a9
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-07 19:45:53 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,14 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   namespace :api, defaults: { format: :json } do
-    resources :users do
-      resources :lists
+    resources :users, only: [:index, :create, :destroy] do
+      resources :lists, only: [:create, :update, :destroy]
     end
-    resources :lists do
-      resources :items
+    resources :lists, only: [:create, :update, :destroy] do
+      resources :items, only: [:create, :update]
     end
-    resources :items
   end
 
-  resources :users do
-    resources :lists
-  end
-
-  resources :lists, only: [] do
-    resources :items, only: [:create, :new]
-  end
-
-  resources :items, only: [:destroy]
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
